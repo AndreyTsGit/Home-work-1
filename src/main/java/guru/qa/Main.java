8f9e586f3f6a9fe2971da386baca533ee7d73d02
Andrey <154456205+AndreyTsGit@users.noreply.github.com>
2024-01-08 19:18:26 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 
 public class Main {
-    private static String msg = "Hello Egor!!!!";
+    private static String msg = "Hye Egor!";
 
     public static void main(String[] args) {
         System.out.println(msg);
