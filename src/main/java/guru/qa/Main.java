a2c269dfd6abfde15079d8433ca1663e1400e0e8
Andrey <154456205+AndreyTsGit@users.noreply.github.com>
2024-01-08 18:58:33 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,7 @@
 package guru.qa;
 
 public class Main {
-    private static String msg = "Hello Egor!!!!"
+    private static String msg = "Hello Egor!!!!";
 
     public static void main(String[] args) {
         System.out.println(msg);
